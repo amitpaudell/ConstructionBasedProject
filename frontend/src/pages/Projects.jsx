caa6468524db439312a730f2fbc457087f2eb12d
Amit Paudel <amitpaudel789@gmail.com>
2025-10-19 14:05:58 +0545

    projects and services section

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -3,7 +3,43 @@ import React from 'react';
 function Projects() {
   return (
     <div>
-      <h1>Projects</h1>
+      <div className="mt-16 container mx-auto flex flex-col space-y-5">
+        <h3 className="mx-auto text-2xl font-bold">Our Projects </h3>
+        <h1 className="mx-auto text-4xl ">
+          Discover our diverse range of projects
+        </h1>
+        <p className="mx-auto text-xl">
+          We offer a diverse array of construction services, spanning
+          residential, commercial, and industrial projects.
+        </p>
+      </div>
+
+      <div>
+        <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+          <div class="relative p-2.5 h-96 overflow-hidden rounded-xl bg-clip-border">
+            <img
+              src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+              alt="card-image"
+              class="h-full w-full object-cover rounded-md"
+            />
+          </div>
+          <div class="p-4">
+            <div class="mb-2 flex items-center justify-between">
+              <p class="text-slate-800 text-xl font-semibold">Apple AirPods</p>
+            </div>
+            <p class="text-slate-600 leading-normal font-light">
+              With plenty of talk and listen time, voice-activated Siri access,
+              and an available wireless charging case.
+            </p>
+            <button
+              class="rounded-md w-full mt-6 bg-cyan-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-cyan-700 focus:shadow-none active:bg-cyan-700 hover:bg-cyan-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+            >
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
